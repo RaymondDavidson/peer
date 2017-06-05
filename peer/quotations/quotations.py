f028fc246c26b435e0c41c41bc17aae1f4ae0c5e
Rik Goldman <rikgoldman@gmail.com>
2017-06-05 05:44:26 -0400

    add stupid quotes for front page

---
+++ peer/quotations/quotations.py
@@ -0,0 +1,11 @@
+quotes = [
+    'A certain change in the emphasis on having and being is apparent in the growing use of nouns and the decreasing use of verbs in Western languages in the past few centuries. A noun is the proper denotation for a thing. I can say that I have things: for instance that I have a table, a house, a book, a car.\nThe proper denotation for an activity, a process, is a verb: for instance I am, I love, I desire, I hate, etc. Yet ever more frequently an activity is expressed in terms of having; that is, a noun is used instead of a verb. But to express an activity by to have in connection with a noun is an erroneous use of language, because processes and activities cannot be possessed; they can only be experienced.\n--Erich Fromm',
+
+    'Throw up into your typewriter every morning. Clean up every noon./n ― Raymond Chandler',
+
+    'The writer must have a good imagination to begin with, but the imagination has to be muscular, which means it must be exercised in a disciplined way, day in and day out, by writing, failing, succeeding and revising.\n--Stephen King',
+
+    'The best advice I can give on this is, once it\'s done, to put it away until you can read it with new eyes. When you\'re ready, pick it up and read it, as if you\'ve never read it before. If there are things you aren\'t satisfied with as a reader, go in and fix them as a writer: that\'s revision.\n --Neil Gaiman'
+
+
+]
